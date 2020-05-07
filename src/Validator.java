bf7addd9e26a8cb8e7329deef5c7dd679ca4f235
syedfaizanalam <43822557+syedfaizanalam@users.noreply.github.com>
2020-05-07 15:42:58 -0400

    update

--- src/Validator.java
+++ src/Validator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
  * Validator() should have methods that: 
  * 1. check for “yes no” 
@@ -6,4 +8,32 @@
  */
 public class Validator {
 	//code
+	
+	public String validation(String word, String letter) {
+		
+		Scanner scnr = new Scanner(System.in);
+		
+		return "";
+	}
+	
+	public static boolean validation (String word, Scanner letter) {
+		String input = letter.next().toLowerCase();
+		boolean isValid = word.contains(input);
+			while (!isValid) { // keep looping as long as they enter something invalid
+				System.out.println("Oops. Try again!.");
+				input = letter.next().toLowerCase();
+				isValid = word.contains(input);
+			}
+		
+		
+		
+		return input.startsWith("y");
+	}
+	
+/*
+//put the for loop in here to validate all the letters
+public static String checker(String x) {
+	for (int i = 0; i < word.length(); i++) {}
+}
+*/
 }
