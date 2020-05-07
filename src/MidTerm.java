a15d377e0d665830755138c766efc635c4e5dcdd
William <btrigona@gmail.com>
2020-05-07 10:30:48 -0400

    NextLine

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -10,6 +10,7 @@ public class MidTerm {
 		System.out.println("Luckily, the lion had friends coming over and his entourage realized their buddy needed backup.");
 		System.out.println("This started an all out battle in Kai-Yun's camp between monkeys and lions.");
 		System.out.println("The monkies, in a last ditch effort, combined their powers and fused into the MEGAULTRANINJAMONKEY!!");
+		System.out.println("The monkies, drunk on their own power, decimated the pitiful lions with powerful punches and kicks.");
 	}
 
 }
