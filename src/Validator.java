ebc60c003b41659a0259e2ea0096e8d9cd7e844a
harley <harleymakesart@gmail.com>
2020-05-07 13:05:32 -0400

    Set up the framework, created the classes

---
+++ src/Validator.java
@@ -0,0 +1,9 @@
+/*
+ * Validator() should have methods that: 
+ * 1. check for “yes no” 
+ * 2. Check that the user entered a letter (not a number, multiple characters, or nothing)
+ * 3. find a letter in a string (Return true for a “hit”, meaning the letter was found in the string. Return False for “miss”, meaning it was not.)
+ */
+public class Validator {
+	//code
+}
