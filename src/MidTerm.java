708a603f41b47700e0a9c5999663e13f8f577425
harley <harleymakesart@gmail.com>
2020-05-07 10:21:03 -0400

    Added my first line.

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -3,7 +3,8 @@ public class MidTerm {
 
 	public static void main(String[] args) {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
-System.out.println("The ninja monkeys decided to attack.");
+		System.out.println("The ninja monkeys decided to attack.");
+		System.out.println("There was a sorcerous lion that walked into camp to steal the supplies.");
 	}
 
 }
