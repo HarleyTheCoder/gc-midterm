f3153381ef4190acbd3da6972be5c10b6fce7032
William <btrigona@gmail.com>
2020-05-07 11:21:24 -0400

    NextChange

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -2,7 +2,7 @@
 public class MidTerm {
 
 	public static void main(String[] args) {
-		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+		System.out.println("Once upon a time in the mystical land of Detroit there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
 		System.out.println("The ninja monkeys decided to attack.");
 		System.out.println("There was a sorcerous lion that walked into camp to steal the supplies.");
 		System.out.println("Fortunately the monkies were very clever, they they swapped the real supplies with bales of marijuana they had bought at market.");
