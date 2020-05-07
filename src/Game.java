5f4c7447fc53254824448f38128682a314c85c3b
harley <harleymakesart@gmail.com>
2020-05-07 14:46:20 -0400

    Added some game methods.

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,53 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 //Store all the game methods here
 public class Game {
-	//code
+	
+	
+	
+	//Store the words into a list
+	public static void storeWords(Path path, List<String> words) {
+		try {
+			if (Files.notExists(path)) {
+				Files.createFile(path);
+			}
+			
+			List<String> temp = Files.readAllLines(path);
+			for (String t: temp) {
+				words.add(t);
+			}
+			
+		} catch (IOException e) {
+			System.out.println("Error loading file.");
+			e.printStackTrace();
+		}
+	}
+	
+	//Select a word to use in the game
+	public static String selectWord(List<String> words) {
+		int num = (int)(Math.random() * words.size());
+		String word = words.get(num);
+		return word;
+	}
+	
+	//Create the text to display what word to solve
+	public static String createEmptyWord (String word) {
+		String emptyWord = "";
+		String blank = "_";
+		for (int i = 0; i < word.length(); i++) {
+			emptyWord += blank + " ";
+		}
+		return emptyWord;
+	}
+	
+	//Guess a letter
+	public static void guessLetter(String word, String letter) {
+		
+	}
+	
+	//
 }
--- src/MidTerm.java
+++ src/MidTerm.java
@@ -1,9 +1,42 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class MidTerm {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
+		List<String> words = new ArrayList<>();
+		String wordToSolve;
 		
+		String blankWord; //This is for the display, it might end up in the
+						  //HangedMan class
+		
+		//Set path for the words text file
+		String dirPath = "C:/Coding/eclipse_workspace/gc-midterm/src/";
+		Path path = Paths.get(dirPath + "HangManWords.txt");
+		
+		//Store words from text file into the list
+		Game.storeWords(path, words);
+		
+		while (true) { //While loop for a new game. Goes until user quits.
+			
+			wordToSolve = Game.selectWord(words); //Pick a word for the game
+			blankWord = Game.createEmptyWord(wordToSolve); //Create empty display
+														   //of the word.
+			
+			
+	System.out.println(wordToSolve + "\n" + blankWord); //testing code
+			
+			
+			break; //temporary for the sake of ending the loop
+		}
+
+	//Close the scanner.
+	scan.close();	
 	}
 
 }
