8c062ff90cb8590e4f7acdca1f9054e4023e4fc4
syedfaizanalam <43822557+syedfaizanalam@users.noreply.github.com>
2020-05-07 10:32:44 -0400

    update

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -10,6 +10,9 @@ public class MidTerm {
 		System.out.println("Luckily, the lion had friends coming over and his entourage realized their buddy needed backup.");
 		System.out.println("This started an all out battle in Kai-Yun's camp between monkeys and lions.");
 		System.out.println("The monkies, in a last ditch effort, combined their powers and fused into the MEGAULTRANINJAMONKEY!!");
+	
+		System.out.println("The lions have a long tradition of training day and night. . .  their training is finally payoff by facing MEGAULTRANINJAMONKEY!!");
+	
 	}
 
 }
